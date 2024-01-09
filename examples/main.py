1efbb1309dcb37fb71c538156fe0a97a5147d011
Franco-MB <edisonfranco20133@gmail.com>
2024-01-09 03:43:14 -0300

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,15 +1,1 @@
-
-from machine import Pin
-import time
-
-led = Pin(2, Pin.OUT)
-
-while(True):
-
-  led.on()
-  print('Led OFF')
-  time.sleep_ms(100)
-
-  led.off()
-  print('Led ON')
-  time.sleep_ms(100)
+print("teste")
