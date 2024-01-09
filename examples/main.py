61f07619c9bd5088c6f368385945357a7a6831c3
Franco-MB <edisonfranco20133@gmail.com>
2024-01-09 04:29:18 -0300

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,14 @@
 from machine import Pin
 import time
+import senko
+
+
+# Verifique se a versão mais recente está disponível
+def version():
+  if OTA.fetch():
+    print("Nova versão disponível")
+    machine.reset()
+
 
 led = Pin(2, Pin.OUT)
 
@@ -12,3 +21,5 @@ while(True):
   led.off()
   print('Led ON')
   time.sleep_ms(100)
+
+  version()
