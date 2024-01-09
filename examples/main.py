850f94e668811a5033aab21037372681073084a3
Franco-MB <edisonfranco20133@gmail.com>
2024-01-09 05:15:55 -0300

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,20 +1,20 @@
-from machine import Pin
+import machine
 import time
 import senko
 
 OTA = senko.Senko(user="Franco-MB", repo="senko", working_dir="examples", files=["main.py"])
 
-led = Pin(2, Pin.OUT)
+led = machine.Pin(2, Pin.OUT)
 
 while(True):
 
   led.on()
   print('Led OFF')
-  time.sleep_ms(2000)
+  time.sleep_ms(5000)
 
   led.off()
   print('Led ON')
-  time.sleep_ms(2000)
+  time.sleep_ms(5000)
 
   # Verifique se a versão mais recente está disponível
   if OTA.fetch():
